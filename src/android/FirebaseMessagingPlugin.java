4e7d959efae60a11fe64b64008c7f8d63cb4b728
OutsystemsNOS <45230356+OutsystemsNOS@users.noreply.github.com>
2020-10-27 17:27:48 +0000

    Update FirebaseMessagingPlugin.java

--- src/android/FirebaseMessagingPlugin.java
+++ src/android/FirebaseMessagingPlugin.java
@@ -2,7 +2,7 @@ package by.chemerisuk.cordova.firebase;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.ContentResolver;
+import android.content;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
