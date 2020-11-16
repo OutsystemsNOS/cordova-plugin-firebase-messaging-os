a08f78cff430c84de5fc0de0c95575c0f17916b4
OutsystemsNOS <45230356+OutsystemsNOS@users.noreply.github.com>
2020-11-16 15:39:26 +0000

    Update FirebaseMessagingPlugin.m

--- src/ios/FirebaseMessagingPlugin.m
+++ src/ios/FirebaseMessagingPlugin.m
@@ -5,6 +5,25 @@
 @import Firebase;
 
 @implementation FirebaseMessagingPlugin
+- (void)hasPermission:(CDVInvokedUrlCommand *)command {
+    BOOL enabled = NO;
+    UIApplication *application = [UIApplication sharedApplication];
+
+    if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        enabled = application.currentUserNotificationSettings.types != UIUserNotificationTypeNone;
+    } else {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+        enabled = application.enabledRemoteNotificationTypes != UIRemoteNotificationTypeNone;
+#pragma GCC diagnostic pop
+    }
+
+    NSMutableDictionary* message = [NSMutableDictionary dictionaryWithCapacity:1];
+    [message setObject:[NSNumber numberWithBool:enabled] forKey:@"isEnabled"];
+    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:message];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];
+}
+
 - (void)requestPermission:(CDVInvokedUrlCommand *)command {
     NSDictionary* options = [command.arguments objectAtIndex:0];
 
